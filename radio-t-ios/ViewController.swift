7fe02f2f2c02d828a211af02c5df2d40e86eea23
Ilsur Valiullin <mail@ivaliullin.com>
2023-05-14 19:55:32 +0300

    Blank project
    Create Xcode project and add gitignore

---
+++ radio-t-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  radio-t-ios
+//
+//  Created by Ilsur Valiullin on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
